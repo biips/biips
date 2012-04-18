4152772550c68f5d95c61b03b78ebaa26730e13e
adrtod <adrien.todeschini@gmail.com>
2012-04-18 15:45:37 +0000

    cleaning sources, license notices, etc
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/biips/trunk@286 d17473f4-ba5e-44ce-b6ba-a5eee1777043

--- testcompiler/bench/backward_smoother_hmm_1d.m
+++ testcompiler/bench/backward_smoother_hmm_1d.m
@@ -1,3 +1,39 @@
+%                                               -*- matlab -*-
+%
+%  BiiPS software is a set of C++ libraries for
+%  Bayesian inference with interacting Particle Systems.
+%  Copyright (C) Inria, 2012
+%  Contributors: Adrien Todeschini, Francois Caron
+%  
+%  BiiPS is derived software based on:
+%  JAGS, Copyright (C) Martyn Plummer, 2002-2010
+%  SMCTC, Copyright (C) Adam M. Johansen, 2008-2009
+%
+%  This file is part of BiiPS.
+%
+%  BiiPS is free software: you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation, either version 3 of the License, or
+%  (at your option) any later version.
+%
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+%
+%  You should have received a copy of the GNU General Public License
+%  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+%
+%  \file     backward_smoother_hmm_1d.m
+%  \brief    
+%
+%  \author   $LastChangedBy$
+%  \date     $LastChangedDate$
+%  \version  $LastChangedRevision$
+%  Id:       $Id$
+%
+
 function [x_est_PS_t, w_PS_t, ESS_t] = backward_smoother_hmm_1d(x_PF_t, x_PF_tplus1, w_PF_t, w_PS_tplus1, t, log_prior_pdf)
 %BACKWARD_SMOOTHER_HMM_1D Summary of this function goes here
 %   Detailed explanation goes here
