57a4b4e8e98cc929e665ad17c65a8273d467729a
aitzkora <marc.fuentes@inria.fr>
2012-07-19 15:23:48 +0000

    matbiips : load_module now returns a boolean
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/biips/trunk@322 d17473f4-ba5e-44ce-b6ba-a5eee1777043

--- matbiips/cono.m
+++ matbiips/cono.m
@@ -1,10 +1,9 @@
 x=load('x.dat');
 y=load('y.dat');
 data=struct('y', y, 'tmax', 100, 'precxinit', 1,'precx', 100, 'precy', 1, 'meanxinit', 0);
-%data=struct('tmax', 100, 'precxinit', 1,'precx', 100, 'precy', 1, 'meanxinit', 0);
-inter_biips('load_module', 'basemod'); 
+bool_et_un = inter_biips('load_module', 'basemod'); 
 p0=inter_biips('make_console'); 
 inter_biips('check_model', p0, 'hmm_1d_lin.bug'); 
 inter_biips('compile_model', p0, data, false, 12);
-%inter_biips('compile_model', p0, data, true, 12);
+debilos = inter_biips('get_data',p0);
 inter_biips('clear_console',p0); 
