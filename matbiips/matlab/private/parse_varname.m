93ea23f476818150faadda8ef7e24c9cfd580745
aitzkora <marc.fuentes@inria.fr>
2012-08-22 09:12:12 +0000

    matbiips : add some test to biips_smc_samples
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/biips/trunk@382 d17473f4-ba5e-44ce-b6ba-a5eee1777043

--- matbiips/matlab/private/parse_varname.m
+++ matbiips/matlab/private/parse_varname.m
@@ -4,8 +4,8 @@ z1=textscan(str, '%s', 'delimiter', '[');
 z1=z1{1};
 s=struct('name', z1{1}); 
 if (length(z1) == 1)
-  s.lower=nan;
-  s.upper=nan;
+  s.lower=cell(0);
+  s.upper=cell(0);
 else
   z2=textscan(z1{2},'%s', 'delimiter',']');
   z2=z2{1}{1};
--- matbiips/tests/test_smc.m
+++ matbiips/tests/test_smc.m
@@ -20,7 +20,7 @@ inter_biips('set_filter_monitors', p0, {'x'} , {1 }, { tmax });
 inter_biips('set_smooth_tree_monitors', p0, {'x'}, {1}, {tmax});
 inter_biips('set_smooth_monitors', p0, {'x'}, {1}, {tmax });
 if (~inter_biips('is_sampler_built',p0))
-  inter_biips('build_smc_sampler',p0, 0);
+  inter_biips('build_smc_sampler',p0, false);
   sort_nodes=inter_biips('get_sorted_nodes', p0)
   node_samplers=inter_biips('get_nodes_samplers', p0)
 end
@@ -31,15 +31,15 @@ end
 log_norm=inter_biips('get_log_norm_const', p0)
 filter_monitors=inter_biips('get_filter_monitors',p0)
 smooth_monitors=inter_biips('get_smooth_tree_monitors',p0)
-inter_biips('clear_smooth_tree_monitors',p0);
+inter_biips('clear_smooth_tree_monitors',p0, false);
 % on fait un backward
 if (~inter_biips('is_smc_sampler_at_end', p0))
    error('sampler not finish!');
 end   
 inter_biips('run_backward_smoother', p0);
-inter_biips('clear_filter_monitors',p0);
+inter_biips('clear_filter_monitors', p0, false);
 backward_smooth_monitors=inter_biips('get_smooth_monitors',p0)
-inter_biips('clear_smooth_monitors',p0);
+inter_biips('clear_smooth_monitors', p0), false;
 % on nettoie la console
 inter_biips('clear_console',p0); 
 exit;
