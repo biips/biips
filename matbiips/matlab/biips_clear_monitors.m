c90b1011d48ca06116c82a6b6e14f7390881ce72
aitzkora <marc.fuentes@inria.fr>
2012-09-03 15:51:21 +0000

    matbiips : correction clear_monitors et smc_samples not yet working
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/biips/trunk@403 d17473f4-ba5e-44ce-b6ba-a5eee1777043

--- matbiips/matlab/biips_clear_monitors.m
+++ matbiips/matlab/biips_clear_monitors.m
@@ -1,13 +1,27 @@
-function biips_clear_monitors(p, type)
-% CLEAR_MONITOR_BIIPS clear some monitors  
-% 
+function biips_clear_monitors(p, type, varargin)
+% BIIPS_CLEAR_MONITORS clear some monitors  
+% biips_clear_monitors(console, type, [release_only])
+% INPUT : 
+% - console : id of the current console
+% - type : string containing 'f' - forward
+%                            's' - smoothing
+%                            'b' - backward smoothing
+% - release_only : boolean flag to indicate what kind of 
+% clearing has to be done
+opt_argin = length(varargin);
+%default values
+release_only = false;
+if opt_argin >= 1
+   sample_data = varargin{1};
+end
+
 indices = arrayfun(@(x) strfind(type, x), 'fsb', 'UniformOutput', 0); 
 if (~isempty(indices{1})) % filtering
-  inter_biips('clear_filter_monitors', p);
+  inter_biips('clear_filter_monitors', p, release_only);
 end 
 if (~isempty(indices{2})) % smoothing
-  inter_biips('clear_smooth_tree_monitors', p);
+  inter_biips('clear_smooth_tree_monitors', p, release_only);
 end  
 if (~isempty(indices{3})) %backward_smoothing
-  inter_biips('clear_smooth_monitors', p);
+  inter_biips('clear_smooth_monitors', p, release_only);
 end 
