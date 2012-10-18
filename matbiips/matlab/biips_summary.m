2effe3025d824bda6e547dccea672c94259b5019
aitzkora <marc.fuentes@inria.fr>
2012-10-18 16:02:58 +0000

    matbiips : summary not yet finished
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/biips/trunk@444 d17473f4-ba5e-44ce-b6ba-a5eee1777043

--- matbiips/matlab/biips_summary.m
+++ matbiips/matlab/biips_summary.m
@@ -8,6 +8,7 @@ summ.mom2 =  sum(part.values.^2 .* part.weights, d);
 summ.mom3 =  sum(part.values.^3 .* part.weights, d); 
 summ.mom4 =  sum(part.values.^4 .* part.weights, d); 
 summ.var = summ.mom2 - summ.mean.^2;
-summ.skew = (summ.mom3 - 3 * summ.mom2 * summ.mean + 2 * summ.mean.^3)./sum.var.^3/2
-%summ.kurt = summ.mom4 - 4* summ.mom3 .* summ.mean
+%summ.skew = (summ.mom3 - 3 .* summ.mom2 .* summ.mean + 2 .* summ.mean.^3)./sum.var.^3/2
+%summ.kurt = (summ.mom4 - 4 .* summ.mom3 .* summ.mean + 6 .* summ.mom2*summ.mean.^2 - 3 * summ.mean.^4)./summ.var.^2 -3
+
 end
