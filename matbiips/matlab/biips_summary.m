0592d012ed1c5cf844304c37ca0069b09acfb6ff
aitzkora <marc.fuentes@inria.fr>
2012-10-23 15:51:09 +0000

    add quantiles in biips_summary
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/biips/trunk@446 d17473f4-ba5e-44ce-b6ba-a5eee1777043

--- matbiips/matlab/biips_summary.m
+++ matbiips/matlab/biips_summary.m
@@ -3,6 +3,7 @@ function [ summ ] = biips_summary( part )
 %   Detailed explanation goes here
 
 d = length(size(part.values));
+size_part = size(part.values);
 summ.mean = sum(part.values .* part.weights, d);
 summ.mom2 =  sum(part.values.^2 .* part.weights, d); 
 summ.mom3 =  sum(part.values.^3 .* part.weights, d); 
@@ -10,5 +11,9 @@ summ.mom4 =  sum(part.values.^4 .* part.weights, d);
 summ.var = summ.mom2 - summ.mean.^2;
 %summ.skew = (summ.mom3 - 3 .* summ.mom2 .* summ.mean + 2 .* summ.mean.^3)./sum.var.^3/2
 %summ.kurt = (summ.mom4 - 4 .* summ.mom3 .* summ.mean + 6 .* summ.mom2*summ.mean.^2 - 3 * summ.mean.^4)./summ.var.^2 -3
-
+cv = num2cell(part.values, d);
+cs=  num2cell(part.weights, d);
+probas = [0.025 0.0975];
+quantiles = cellfun(@(w,x) inter_biips('weighted_quantiles', w, x, probas), cv, cs, 'UniformOutput',0);
+summ.quants=reshape(cat(3, quantiles{:}), [length(probas) size_part(1:d-1)]);
 end
