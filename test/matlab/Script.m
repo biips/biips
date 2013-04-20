fd019751b6db7c4051521a7f4a51875024f1990c
aitzkora <marc.fuentes@inria.fr>
2013-04-20 14:50:10 +0000

    on a re-ajoute l'impression dans les fichiers pour sauvegarder la configuration
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/biips/trunk@519 d17473f4-ba5e-44ce-b6ba-a5eee1777043

--- test/matlab/Script.m
+++ test/matlab/Script.m
@@ -1,11 +1,11 @@
 for x ='a':'j'
     % HMM 1D linear gaussian model
     filename = sprintf('hmm_1d_lin_gauss.%s.cfg', x);
-    run_bench_all(1, [100, 500, 1000], 0.5, 10000, {filename});
-    copyfile(filename, '../benchmarks/')
+    run_bench_all(1, [100], 0.5, 10000, {filename});
+    movefile(filename, '../benchmarks')
 
     % HMM 1D non linear gaussian model
     filename = sprintf('hmm_1d_non_lin_gauss.%s.cfg', x);
-    run_bench_all(2, [100, 500, 1000], 0.5, 10000, {filename});
-    copyfile(filename, '../benchmarks/')
+    run_bench_all(2, [100], 0.5, 10000, {filename});
+    movefile(filename, '../benchmarks')
 end
