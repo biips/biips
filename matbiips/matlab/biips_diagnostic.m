7223a962373e9fc3969ca68b9f475d2779469b6a
Francois Caron <caron@stats.ox.ac.uk>
2014-01-24 15:58:42 +0000

    update matbiips and matlab examples

--- matbiips/matlab/biips_diagnostic.m
+++ matbiips/matlab/biips_diagnostic.m
@@ -1,37 +1,53 @@
 function [diagn] = biips_diagnostic(parts, varargin)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % BIIPS_DIAGNOSTIC returns a diagnostic structure
-% diagn = biips_diagnostic(particles, [variables, fsb, ess_thres, quiet])
+% diagn = biips_diagnostic(parts, 'Propertyname', propertyvalue, ...)
+%
 % INPUT
-% -particles : input structure containing the particles of different variables.
-%              usually returned by biips_smc_samples function
-% -variables : cell of strings. subset of the fields of particles struct
-%              argument
-% -fsb : string containing the characters 'f', 's' and/or 'b'
-% -ess_thres : integer. Threshold on the Effective Sample Size (ESS) of the
-%              examined particles. If all the ESS components are over the
-%              threshold, the diagnostic is valid, otherwise it is not
-%              valid.
-%              default is 30
-% -quiet : flag. deactivate message display. default is 0
+% -parts:      input structure containing the particles of different variables.
+%               usually returned by biips_smc_samples function
+% Optional Inputs:
+% -vars:       cell of strings. subset of the fields of particles struct
+%               argument
+% -fsb:        string containing the characters 'f', 's' and/or 'b'
+% -ess_thres :  integer. Threshold on the Effective Sample Size (ESS) of the
+%               examined particles. If all the ESS components are over the
+%               threshold, the diagnostic is valid, otherwise it is not
+%               valid.
+%               default is 30
+% -quiet:      flag. deactivate message display. default is 0
+%
 % OUTPUT
-% -diagn : output structure
-opt_argin = length(varargin);
+% -diagn:      output structure providing the minimum value of the
+%              effective sample size
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% EXAMPLE:
+% data = struct('var1', 0, 'var2', 1.2);
+% model_id = biips_model('model.bug', data)
+% npart = 100; variables = {'x'}; 
+% out_smc = biips_smc_samples(model_id, variables, npart);
+% diag = biips_diagnostic(out_smc);% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% BiiPS Project - Bayesian Inference with interacting Particle Systems
+%
+% Reference: A. Todeschini, M. Fuentes, F. Caron, P. Legrand, P. Del Moral.
+% BiiPS: a software for Bayesian inference with interacting particle
+% systems. Technical Report, INRIA. February 2014.
+%
+% Authors: Adrien Todeschini, Marc Fuentes
+% INRIA Bordeaux, France
+% email: biips-project@lists.gforge.inria.fr
+% Website: https://alea.bordeaux.inria.fr/biips
+% Jan 2014; Last revision: 24-01-2014
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Default values
 vars = {};
-if opt_argin >=1
-    vars = varargin{1};
-end
 fsb = '';
-if opt_argin >=2
-    fsb = varargin{2};
-end
 ess_thres = 30;
-if opt_argin >=3
-    ess_thres = varargin{3};
-end
 quiet = 0;
-if opt_argin >=4
-    quiet = varargin{4};
-end
+parsevar; % Process options
 
 if (isempty(vars))
     vars = fieldnames(parts); % vars = {}, take all fields
@@ -43,7 +59,7 @@ if (isempty(fsb)) % retrieve only the field presents in the first variable
     indices=horzcat(indices{:});
     fsb=chaine(sort(indices));
 end
-% select only the wanted variables
+% Select only the wanted variables
 s = biips_cell2struct(cellfun(@(x) getfield(parts, x), vars,'UniformOutput',0), vars);
 cell_fsb = num2cell(fsb);
 cell_diagn = cell(size(vars));
