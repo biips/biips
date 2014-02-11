c555428a157caa40b4bcdedb925021d570dce712
Francois Caron <caron@stats.ox.ac.uk>
2014-02-11 08:30:31 +0000

    matbiips: correct typo biips_pimh_samples.m - add nonlin hmm example

--- matbiips/tests/test_matbiips.m
+++ matbiips/tests/test_matbiips.m
@@ -21,18 +21,18 @@ data = struct('t_max', t_max, 'prec_x_init', prec_x_init,...
 %%% Start BiiPS console
 biips_init;
 %%% Compile BUGS model and sample data
-model = 'hmm_1d_lin.bug'; % BUGS model filename
+model = 'hmm_1d_linv2.bug'; % BUGS model filename
 sample_data = true; % Boolean
 [model_id, data] = biips_model(model, data, 'sample_data', sample_data);
 
 % Test model with cell data structure
-t_max = 10; mean_x_init = 0;prec_x_init = 1;prec_x = 1;prec_y = 10;
+t_max = 40; mean_x_init = 0;prec_x_init = 1;prec_x = 1;prec_y = 10;
 data = {'t_max', 'prec_x_init',...
     'prec_x', 'prec_y', 'mean_x_init'};
 %%% Start BiiPS console
 biips_init;
 %%% Compile BUGS model and sample data
-model = 'hmm_1d_lin.bug'; % BUGS model filename
+% model = 'hmm_1d_lin.bug'; % BUGS model filename
 sample_data = true; % Boolean
 [model_id, data] = biips_model(model, data, 'sample_data', sample_data);
 
@@ -70,10 +70,10 @@ catch err
 end
 
 %% TESTS BIIPS_SMC_SAMPLES
-n_part = 10; % Number of particles
+n_part = 20; % Number of particles
 variables = {'x', 'x[1:2]', 'unknownvariable'}; % Variables to be monitored
 type = 'fs'; rs_type = 'stratified'; rs_thres = 0.5; % Optional parameters
 % Run SMC
 out_smc = biips_smc_samples(model_id, variables, n_part,...
     'type', type, 'rs_type', rs_type, 'rs_thres', rs_thres);
-
+nodes = biips_get_nodes(model_id)
