301deb1540b41c2d7c3d9d42830fe536b6d1c349
Francois Caron <caron@stats.ox.ac.uk>
2014-03-12 20:53:55 +0000

    fix problem with dimensions in pmmh functions learn_cov and rw_proposal add test multiD pmmh in test_matbiips.asv add test_crash4 with problem to solve in change_data

---
+++ matbiips/tests/test_crash4.m
@@ -0,0 +1,39 @@
+%% TESTS BIIPS_PMMH
+% Test model with cell data structure
+t_max = 20; mean_x_init = 0;prec_x_init = 1;prec_x = 1; mean0 = [0;0]; prec0 = eye(2);
+data = {'t_max', 'prec_x_init',...
+    'prec_x', 'mean_x_init', 'mean0', 'prec0'};
+%%% Start BiiPS console
+biips_init;
+%%% Compile BUGS model and sample data
+model = 'hmm_1d_nonlin_param2.bug'; % BUGS model filename
+sample_data = true; % Boolean
+[model_id, data] = biips_model(model, data, 'sample_data', sample_data);
+
+param_names = {'log_prec_y[1:2]','log_prec_y2[1:2]'};
+inits = {[0;0], [0;0]};
+latent_names = {};
+n_burn = 500;
+n_part = 50;
+n_iter = 520;
+%%
+% *Init PMMH*
+obj_pmmh = biips_pmmh_object(model_id, param_names, 'inits', inits); % creates a pmmh object
+
+%%
+% *Run PMMH*
+obj_pmmh = biips_pmmh_update(obj_pmmh, n_burn, n_part); % adaptation and burn-in iterations
+[out_pmmh, log_post, log_marg_like, stats_pmmh] = biips_pmmh_samples(obj_pmmh, n_iter, n_part,...
+    'thin', 1, 'latent_names', latent_names); % Samples
+    
+%    Warning: RUNTIME ERROR: Can not change data: variable log_prec_y[1:2]
+% does not match one node exactly.
+%  
+% Error using biips_pmmh_object>set_param (line 105)
+% Data change failed: invalid initial value for variable log_prec_y
+% 
+% Error in biips_pmmh_object (line 42)
+% sample_param = set_param(console, pn_param, inits);
+% 
+% Error in test_crash4 (line 21)
+%     obj_pmmh = biips_pmmh_object(model_id, param_names, 'inits', inits);
--- matbiips/tests/test_matbiips.m
+++ matbiips/tests/test_matbiips.m
@@ -1,79 +1,115 @@
 close all
 clear all
 
-% RUNS A SET OF TESTS
+test = 'all';% smc, pmmh or all
 
-% Test console
-N=10;
-ids=zeros(N,1);
-for i=1:N;
-  ids(i)=inter_biips('make_console');
+switch(test)
+    case {'smc', 'all'}
+    % RUNS A SET OF TESTS
+
+    % Test console
+    N=10;
+    ids=zeros(N,1);
+    for i=1:N;
+      ids(i)=inter_biips('make_console');
+    end
+    p=randperm(N);
+    for i=1:N
+       inter_biips('clear_console',ids(p(i)));
+    end
+
+    % Test model
+    t_max = 10; mean_x_init = 0;prec_x_init = 1;prec_x = 1;prec_y = 10;
+    data = struct('t_max', t_max, 'prec_x_init', prec_x_init,...
+        'prec_x', prec_x,  'prec_y', prec_y, 'mean_x_init', mean_x_init);
+    %%% Start BiiPS console
+    biips_init;
+    %%% Compile BUGS model and sample data
+    model = 'hmm_1d_lin_mat.bug'; % BUGS model filename
+    sample_data = true; % Boolean
+    [model_id, data] = biips_model(model, data, 'sample_data', sample_data);
+
+    % Test model with cell data structure
+    t_max = 40; mean_x_init = 0;prec_x_init = 1;prec_x = 1;prec_y = 10;
+    data = {'t_max', 'prec_x_init',...
+        'prec_x', 'prec_y', 'mean_x_init'};
+    %%% Start BiiPS console
+    biips_init;
+    %%% Compile BUGS model and sample data
+    % model = 'hmm_1d_lin.bug'; % BUGS model filename
+    sample_data = true; % Boolean
+    [model_id, data] = biips_model(model, data, 'sample_data', sample_data);
+
+
+    % test model with wrong type for argument
+    data = {'t_max', 'prec_x_init',...
+        'prec_x', 'prec_y', 'mean_x_init'};
+    sample_data = 'tartealacreme';
+    [model_id, data] = biips_model(model, data, 'sample_data', sample_data);
+
+    % MAKES THE PROGRAMM CRASH
+    % % Test model with cell data structure
+    % data = {'t_max', ...
+    %     'prec_x', 'prec_y', 'mean_x_init'};
+    % %%% Start BiiPS console
+    % biips_init;
+    % %%% Compile BUGS model and sample data
+    % model = 'hmm_1d_lin.bug'; % BUGS model filename
+    % sample_data = true; % Boolean
+    % [model_id, data] = biips_model(model, data, 'sample_data', sample_data);
+
+
+    % Test model with unknown file
+    badmodel = 'an_unknown_bug_file.bug'; % BUGS model filename
+    try(biips_model(badmodel, data, 'sample_data', sample_data))
+    catch err
+        fprintf(err.message)    
+    end
+
+    % Test model with bad filename
+    badmodel2 = 2; % BUGS model filename
+    try(biips_model(badmodel2, data, 'sample_data', sample_data))
+    catch err
+        fprintf(err.message)
+    end
+
+    %% TESTS BIIPS_SMC_SAMPLES
+    n_part = 20; % Number of particles
+    variables = {'x', 'x[1:2]', 'unknownvariable'}; % Variables to be monitored
+    type = 'fs'; rs_type = 'stratified'; rs_thres = 0.5; % Optional parameters
+    % Run SMC
+    out_smc = biips_smc_samples(model_id, variables, n_part,...
+        'type', type, 'rs_type', rs_type, 'rs_thres', rs_thres);
+    nodes = biips_get_nodes(model_id)
 end
-p=randperm(N);
-for i=1:N
-   inter_biips('clear_console',ids(p(i)));
+switch(test)
+    case {'pmmh', 'all'}
+    
+    %% TESTS BIIPS_PMMH
+    % Test model with cell data structure
+    t_max = 20; mean_x_init = 0;prec_x_init = 1;prec_x = 1; mean0 = [0;0]; prec0 = eye(2);
+    data = {'t_max', 'prec_x_init',...
+        'prec_x', 'mean_x_init', 'mean0', 'prec0'};
+    %%% Start BiiPS console
+    biips_init;
+    %%% Compile BUGS model and sample data
+    model = 'hmm_1d_nonlin_param2.bug'; % BUGS model filename
+    sample_data = true; % Boolean
+    [model_id, data] = biips_model(model, data, 'sample_data', sample_data);
+
+    param_names = {'log_prec_y[1]', 'log_prec_y[2]','log_prec_y2[1:2]', 'alpha[1:1]'};
+    inits = {0, 0, [0;0], 5};
+    latent_names = {};
+    n_burn = 500;
+    n_part = 50;
+    n_iter = 520;
+    %%
+    % *Init PMMH*
+    obj_pmmh = biips_pmmh_object(model_id, param_names, 'inits', inits); % creates a pmmh object
+
+    %%
+    % *Run PMMH*
+    obj_pmmh = biips_pmmh_update(obj_pmmh, n_burn, n_part); % adaptation and burn-in iterations
+    [out_pmmh, log_post, log_marg_like, stats_pmmh] = biips_pmmh_samples(obj_pmmh, n_iter, n_part,...
+        'thin', 1, 'latent_names', latent_names); % Samples
 end
-
-% Test model
-t_max = 10; mean_x_init = 0;prec_x_init = 1;prec_x = 1;prec_y = 10;
-data = struct('t_max', t_max, 'prec_x_init', prec_x_init,...
-    'prec_x', prec_x,  'prec_y', prec_y, 'mean_x_init', mean_x_init);
-%%% Start BiiPS console
-biips_init;
-%%% Compile BUGS model and sample data
-model = 'hmm_1d_lin_mat.bug'; % BUGS model filename
-sample_data = true; % Boolean
-[model_id, data] = biips_model(model, data, 'sample_data', sample_data);
-
-% Test model with cell data structure
-t_max = 40; mean_x_init = 0;prec_x_init = 1;prec_x = 1;prec_y = 10;
-data = {'t_max', 'prec_x_init',...
-    'prec_x', 'prec_y', 'mean_x_init'};
-%%% Start BiiPS console
-biips_init;
-%%% Compile BUGS model and sample data
-% model = 'hmm_1d_lin.bug'; % BUGS model filename
-sample_data = true; % Boolean
-[model_id, data] = biips_model(model, data, 'sample_data', sample_data);
-
-
-% test model with wrong type for argument
-data = {'t_max', 'prec_x_init',...
-    'prec_x', 'prec_y', 'mean_x_init'};
-sample_data = 'tartealacreme';
-[model_id, data] = biips_model(model, data, 'sample_data', sample_data);
-
-% MAKES THE PROGRAMM CRASH
-% % Test model with cell data structure
-% data = {'t_max', ...
-%     'prec_x', 'prec_y', 'mean_x_init'};
-% %%% Start BiiPS console
-% biips_init;
-% %%% Compile BUGS model and sample data
-% model = 'hmm_1d_lin.bug'; % BUGS model filename
-% sample_data = true; % Boolean
-% [model_id, data] = biips_model(model, data, 'sample_data', sample_data);
-
-
-% Test model with unknown file
-badmodel = 'an_unknown_bug_file.bug'; % BUGS model filename
-try(biips_model(badmodel, data, 'sample_data', sample_data))
-catch err
-    fprintf(err.message)    
-end
-
-% Test model with bad filename
-badmodel2 = 2; % BUGS model filename
-try(biips_model(badmodel2, data, 'sample_data', sample_data))
-catch err
-    fprintf(err.message)
-end
-
-%% TESTS BIIPS_SMC_SAMPLES
-n_part = 20; % Number of particles
-variables = {'x', 'x[1:2]', 'unknownvariable'}; % Variables to be monitored
-type = 'fs'; rs_type = 'stratified'; rs_thres = 0.5; % Optional parameters
-% Run SMC
-out_smc = biips_smc_samples(model_id, variables, n_part,...
-    'type', type, 'rs_type', rs_type, 'rs_thres', rs_thres);
-nodes = biips_get_nodes(model_id)
