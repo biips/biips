fd7bc0eecf318eb76a16e60faf25d8d92d049c68
Francois Caron <caron@stats.ox.ac.uk>
2014-03-13 11:13:34 +0000

    remove seed option in matbiips SMC and PIMH rename matbiips functions pimh_init and pimh_one_update add headers for pimh matbiips functions

--- matbiips/matlab/biips_pimh_samples.m
+++ matbiips/matlab/biips_pimh_samples.m
@@ -1,32 +1,68 @@
 function [out, log_marg_like_st] = biips_pimh_samples(console, variable_names, n_iter, n_part, varargin)
 
-%% FC: NEED HEADER
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% BIIPS_PIMH_UPDATE Performs burn-in iterations for the particle
+% independent Metropolis-Hastings algorithm
+% biips_pimh_update(console, variable_names, n_iter, n_part, varargin)
+%
+% INPUT: 
+% - console:     integer. Id of the console containing the model, 
+%                returned by the 'biips_model' function
+% - variable_names: cell of strings. Contains the names of the 
+%                   unobserved variables to monitor.
+%                   Possible value: {'var1', 'var2[1]', 'var3[1:10]',
+%                                                       'var4[1, 5:10, 3]'}
+%                   Dimensions and indices must be a valid subset of 
+%                   the variables of the model.
+% - n_iter:     positive integer. Number of burn-in iterations
+% - n_part:     positive integer. Number of particles used in SMC algorithms
+%
+% Optional Inputs:
+% - thin :      positive integer. Returns samples every thin iterations
+%               (default=1)
+% - rs_thres :  positive real (default = 0.5).
+%               Threshold for the resampling step (adaptive SMC).
+%               if rs_thres is in [0,1] --> resampling occurs when 
+%                                           (ESS > rs_thres * nb_part)
+%               if rs_thres is in [2,nb_part] --> resampling occurs when 
+%                                               (ESS > rs_thres)
+% - rs_type :   string (default = 'stratified')
+%               Possible values are 'stratified', 'systematic', 'residual', 'multinomial'
+%               Indicates the type of algorithm used for the resampling step.
+%
+% OUTPUT:
+% out:          Structure with the PIMH samples for each variable
+% log_marg_like_st: vector with log marginal likelihood over iterations
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% EXAMPLE:
+% data = struct('var1', 0, 'var2', 1.2);
+% model_id = biips_model('model.bug', data)
+% npart = 100; variables = {'x'}; 
+% thin = 2;
+% out_smc = biips_pimh_samples(model_id, variables, npart, 'thin', thin);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% BiiPS Project - Bayesian Inference with interacting Particle Systems
+% MatBiips interface
+% Authors: Adrien Todeschini, Marc Fuentes, François Caron
+% Copyright: INRIA
+% Jan 2014; Last revision: 13-03-2014
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% PROCESS AND CHECK INPUTS
-% CAN WE ADD A CHECK ON THE VARIABLES??
-%%% Process and check optional arguments
-optarg_names = {'rs_thres', 'rs_type', 'seed', 'thin'};
-optarg_default = {.5, 'stratified', get_seed(), 1};
+optarg_names = {'rs_thres', 'rs_type', 'thin'};
+optarg_default = {.5, 'stratified', 1};
 optarg_valid = {[0, n_part],...
-    {'multinomial', 'stratified', 'residual', 'systematic'},...
-    [0, intmax], [1, n_iter]};
-optarg_type = {'numeric', 'char', 'numeric', 'numeric'};
-[rs_thres, rs_type, seed, thin] = parsevar(varargin, optarg_names, optarg_type,...
+    {'multinomial', 'stratified', 'residual', 'systematic'},[1, n_iter]};
+optarg_type = {'numeric', 'char', 'numeric'};
+[rs_thres, rs_type, thin] = parsevar(varargin, optarg_names, optarg_type,...
     optarg_valid, optarg_default);
-% 
-% %% Process options
-% rs_type = 'stratified';
-% rs_thres = 0.5;
-% thin = 1;
-% seed = get_seed();
-% parsevar; % Process options
 
 %% Stops biips verbosity
 inter_biips('verbosity', 0);
 cleanupObj = onCleanup(@() inter_biips('verbosity', 1));% set verbosity on again when function terminates
 
 %% Initialization
-[sample, log_marg_like] = init_pimh(console, variable_names, n_part, rs_thres, rs_type, seed);
+[sample, log_marg_like] = pimh_init(console, variable_names, n_part, rs_thres, rs_type);
 inter_biips('message', ['Generating PIMH samples with ' num2str(n_part)...
     ' particles and ' num2str(n_iter) ' iterations']);
 bar = inter_biips('make_progress_bar', n_iter, '*', 'iterations');
@@ -43,7 +79,7 @@ log_marg_like_st = zeros(n_samples, 1);
 
 % Independent Metropolis-Hastings iterations
 for i=1:n_iter
-    [sample, log_marg_like, accepted] = one_update_pimh(console, ...
+    [sample, log_marg_like, accepted] = pimh_one_update(console, ...
         variable_names, n_part, rs_thres, rs_type, sample, log_marg_like);
     
     % Store output
@@ -51,7 +87,7 @@ for i=1:n_iter
         ind_sample = (i-1)/thin + 1;
         log_marg_like_st(ind_sample) = log_marg_like;
         for k=1:length(variable_names)
-            switch(n_dim(k)) % !!!FC: NOT VERY ELEGANT PIECE OF CODE!!!
+            switch(n_dim(k))
                 case 1
                     samples_st{k}(:, ind_sample) = getfield(sample, variable_names{k});
                 case 2
@@ -84,4 +120,3 @@ for k=1:length(variable_names) % Remove singleton dimensions for vectors
     end
 end
 out = cell2struct_weaknames(samples_st, variable_names);
-% out.log_marg_like = log_marg_like_st;
---
+++ matbiips/matlab/private/pimh_init.m
@@ -0,0 +1,70 @@
+function [sample, log_marg_like] = pimh_init(console, variable_names, n_part, rs_thres, rs_type)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% PIMH_INIT Initialisation of the Particle Independent Metropolis
+% Hastings algorithm
+% sample = pimh_init(console, variable_names, n_part, rs_thres, rs_type)
+% INPUT: 
+% - console :           integer. Id of the console containing the model, 
+%                       returned by the 'biips_model' function
+% - variable_names :    cell of strings. Contains the names of the 
+%                       unobserved variables to monitor.
+%                       Possible value: {'var1', 'var2[1]', 'var3[1:10]',
+%                                                       'var4[1, 5:10, 3]'}
+%                       Dimensions and indices must be a valid subset of 
+%                       the variables of the model.
+% - n_part :            positive integer. Number of particles used in SMC algorithms
+%
+% - rs_thres :  positive real 
+%               Threshold for the resampling step (adaptive SMC).
+%               if rs_thres is in [0,1] --> resampling occurs when 
+%                                           (ESS > rs_thres * nb_part)
+%               if rs_thres is in [2,nb_part] --> resampling occurs when 
+%                                               (ESS > rs_thres)
+% - rs_type :   string 
+%               Possible values are 'stratified', 'systematic', 'residual', 'multinomial'
+%               Indicates the type of algorithm used for the resampling step.           
+
+% OUTPUT:
+% sample:       sampled value from the SMC output
+% log_marg_like:log marginal likelihood
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% BiiPS Project - Bayesian Inference with interacting Particle Systems
+% MatBiips interface
+% Authors: Adrien Todeschini, Marc Fuentes, François Caron
+% Copyright: INRIA
+% Jan 2014; Last revision: 13-03-2014
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%
+monitored = is_monitored(console, variable_names, 's', false);
+if ~monitored
+    % monitor variables
+    monitor_biips(console, variable_names, 's'); 
+end
+
+if (~inter_biips('is_sampler_built', console))
+   inter_biips('build_smc_sampler', console, false);
+end
+
+atend = inter_biips('is_smc_sampler_at_end', console);
+% Get the normalizing constant
+if (~monitored || ~atend)
+    % Run SMC sampler
+    inter_biips('message', 'Initializing PIMH');
+    run_smc_forward(console, n_part, rs_thres, rs_type, get_seed());
+end
+log_marg_like = inter_biips('get_log_norm_const', console);
+
+% Get sampled value
+sampled_value = inter_biips('get_sampled_gen_tree_smooth_particle', console);
+if (isempty(fieldnames(sampled_value)))
+    % Sample one particle
+    inter_biips('sample_gen_tree_smooth_particle', console, get_seed());
+    sampled_value = inter_biips('get_sampled_gen_tree_smooth_particle', console);
+end
+cell_struct = cell(length(variable_names), 1);
+for i=1:length(variable_names)
+    cell_struct{i} = getfield(sampled_value, variable_names{i});
+end
+sample = cell2struct_weaknames(cell_struct, variable_names);
