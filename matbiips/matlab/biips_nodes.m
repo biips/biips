b7ff3fba297ee491624bc535f9cb54e86339c4a2
adrtod <adrien.todeschini@gmail.com>
2014-04-18 17:18:29 +0200

    few renamings in matbiips

---
+++ matbiips/matlab/biips_nodes.m
@@ -0,0 +1,25 @@
+function nodes = biips_nodes(model)
+
+%
+% BIIPS_NODES returns the nodes of the current model
+% nodes = biips_nodes(model)
+%
+%   INPUT
+%   - model:        structure containing the model, 
+%                   returned by the 'biips_model' function
+%
+%   OUTPUT
+%   - nodes: structure containing the nodes
+%
+%   See also BIIPS_MODEL
+%--------------------------------------------------------------------------
+
+% BiiPS Project - Bayesian Inference with interacting Particle Systems
+% MatBiips interface
+% Authors: Adrien Todeschini, Marc Fuentes, Fran�ois Caron
+% Copyright (C) Inria
+% License: GPL-3
+% Jan 2014; Last revision: 18-03-2014
+%--------------------------------------------------------------------------
+
+nodes = matbiips('get_sorted_nodes', model.id);
