fafecdae50d75fbcecf969fc673a5fc01382cf60
adrtod <adrien.todeschini@gmail.com>
2014-07-23 11:08:24 +0200

    fix biips_density bandwith for smoothing

--- matbiips/matlab/biips_density.m
+++ matbiips/matlab/biips_density.m
@@ -19,9 +19,9 @@ function [dens] = biips_density(samples, varargin)
 % EXAMPLE:
 % data = struct('var1', 0, 'var2', 1.2);
 % model_id = biips_model('model.bug', data)
-% n_part = 100; variables = {'x'}; 
+% n_part = 100; variables = {'x'};
 % out_smc = biips_smc_samples(model_id, variables, n_part);
-% kde_estimates = biips_density(out_smc); 
+% kde_estimates = biips_density(out_smc);
 %
 % n_iter = 100;
 % out_pimh = biips_pimh_samples(model_id, variables, n_iter, n_part);
@@ -30,7 +30,7 @@ function [dens] = biips_density(samples, varargin)
 
 % BiiPS Project - Bayesian Inference with interacting Particle Systems
 % MatBiips interface
-% Authors: Adrien Todeschini, Marc Fuentes, François Caron
+% Authors: Adrien Todeschini, Marc Fuentes, Franï¿½ois Caron
 % Copyright (C) Inria
 % License: GPL-3
 % Jan 2014; Last revision: 17-03-2014
@@ -46,38 +46,62 @@ optarg_type = {'char', 'char', 'numeric', 'numeric'};
     optarg_valid, optarg_default);
 
 if (isempty(variable_names))
-   variable_names = fieldnames(samples); % take all fields
+    variable_names = fieldnames(samples); % take all fields
 end
 
 %% Density estimates
 if isstruct(getfield(samples, variable_names{1})) % samples corresponds to the output of a SMC algorithm
-
-	if (isempty(type))
-	    type='fsb';
-	end
-	
-	% retrieve only the field presents in the first variable
-	present = fieldnames(getfield(samples, variable_names{1}));
-	present = strcat(present{:});
-	indices = arrayfun(@(x) strfind(present, x), type, 'UniformOutput', 0);
-	indices = sort([indices{:}]);
-	type = present(indices);
-
+    
+    if (isempty(type))
+        type='fsb';
+    end
+    
+    % retrieve only the field presents in the first variable
+    present = fieldnames(getfield(samples, variable_names{1}));
+    present = strcat(present{:});
+    indices = arrayfun(@(x) strfind(present, x), type, 'UniformOutput', 0);
+    indices = sort([indices{:}]);
+    type = present(indices);
+    
     % select only the wanted variables
     s = cell2struct_weaknames(cellfun(@(x) getfield(samples, x), variable_names,...
         'UniformOutput',0), variable_names);
     cell_fsb = num2cell(type);
     cell_sum = cell(size(variable_names));
-
+    
     for i=1:numel(variable_names)
-      ctemp = cell(size(type));
-      for j=1:numel(type)
-       particles = getfield(getfield(s, variable_names{i}), type(j));
-       size_curr = size(particles.values);
-       d = numel(size_curr);
-       ctemp{j} = cellfun(@(x,w) kde(x, w, adjust, bw), num2cell(particles.values, d), num2cell(particles.weights, d));  
-      end
-      cell_sum{i} = cell2struct_weaknames(ctemp, cell_fsb);
+        ctemp = cell(size(type));
+        % first treat filtering and backward smoothing
+        bw_s = bw;
+        j = strfind(type,'f');
+        if j
+            particles = getfield(getfield(s, variable_names{i}), type(j));
+            size_curr = size(particles.values);
+            d = numel(size_curr);
+            ctemp{j} = cellfun(@(x,w) kde(x, w, adjust, bw), num2cell(particles.values, d), num2cell(particles.weights, d));
+            bw_s = {ctemp{j}.bw};
+        end
+        j = strfind(type,'b');
+        if j
+            particles = getfield(getfield(s, variable_names{i}), type(j));
+            size_curr = size(particles.values);
+            d = numel(size_curr);
+            ctemp{j} = cellfun(@(x,w) kde(x, w, adjust, bw), num2cell(particles.values, d), num2cell(particles.weights, d));
+            bw_s = {ctemp{j}.bw};
+        end
+        % then treat smoothing (applying previous bandwidth if any)
+        j = strfind(type,'s');
+        if j
+            particles = getfield(getfield(s, variable_names{i}), type(j));
+            size_curr = size(particles.values);
+            d = numel(size_curr);
+            if iscell(bw_s)
+                ctemp{j} = cellfun(@(x,w,bw) kde(x, w, 1, bw), num2cell(particles.values, d), num2cell(particles.weights, d), bw_s(:));
+            else
+                ctemp{j} = cellfun(@(x,w) kde(x, w, adjust, bw), num2cell(particles.values, d), num2cell(particles.weights, d));
+            end
+        end
+        cell_sum{i} = cell2struct_weaknames(ctemp, cell_fsb);
     end
     dens = cell2struct_weaknames(cell_sum, variable_names);
 else % samples corresponds to the output of a MCMC algorithm
@@ -87,11 +111,11 @@ else % samples corresponds to the output of a MCMC algorithm
     nsamples = size(getfield(s, variable_names{1}), ndims(getfield(s, variable_names{1})));
     cell_sum = cell(size(variable_names));
     for i=1:numel(variable_names)
-      samp = getfield(s, variable_names{i});
-      weights = 1/nsamples * ones(size(samp));
-      size_curr = size(samp);
-       d = numel(size_curr);
-      cell_sum{i} = cellfun(@(x,w) kde(x, w, adjust, bw), num2cell(samp, d), num2cell(weights, d));
+        samp = getfield(s, variable_names{i});
+        weights = 1/nsamples * ones(size(samp));
+        size_curr = size(samp);
+        d = numel(size_curr);
+        cell_sum{i} = cellfun(@(x,w) kde(x, w, adjust, bw), num2cell(samp, d), num2cell(weights, d));
     end
     dens = cell2struct_weaknames(cell_sum, variable_names);
 end
