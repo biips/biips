8cc9562ac6e29d7a2da89c899f858a120968479a
adrtod <adrien.todeschini@gmail.com>
2014-08-11 02:08:53 +0200

    move matbiips/matlab content to matbiips

--- examples/object_tracking/hmm_4d_nonlin.m
+++ examples/object_tracking/hmm_4d_nonlin.m
@@ -74,7 +74,7 @@ rng('default')
 % and add the Matbiips folder to the Matlab path
 % 
 
-matbiips_path = '../../matbiips/matlab';
+matbiips_path = '../../matbiips';
 addpath(matbiips_path)
 
 %% Load model and data
